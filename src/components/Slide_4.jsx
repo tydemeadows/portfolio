9195efb3eb00dccc5f68f7a8af4b51cf809ceba2
Tyde Meadows <tyde.meadows@gmail.com>
2022-08-20 12:07:15 -0500

    nice progress

--- src/components/Slide_4.jsx
+++ src/components/Slide_4.jsx
@@ -2,17 +2,31 @@ import './Slide_4.css';
 import React, {useRef, useEffect, useState} from 'react'
 import FaroCard from "./FaroCard";
 import NasaMdaCard from "./NasaMdaCard";
+import faro_img from "./FARO_transparent_bg_w700px.png";
+import nasa_img from "./nasa-logo-white_fill.png";
 
-const Slide_1 = () => {
+const Slide_4 = () => {
   return (
     <div id="slide-4" className="slideobj">
-      <div id='pro-id' className="pro-holder">
-       <FaroCard/>
-       <NasaMdaCard/>
-       </div>
+      <div id='inlay'>
+      <div id='job_img_1'>
+          <a href="https://www.faro.com/en" target="_blank">
+          <img src={faro_img} id="faro_bg_img" className="pastbg" />
+          </a>
+        </div>
+      <div id='job_img_2'>
+          <a href="https://www.nasa.gov/" target="_blank">
+          <img src={nasa_img} id="nasa_bg_img" className="pastbg" />
+          </a>
+        </div>
+        <div id='pro-id' className="pro-holder">
+          <FaroCard/>
+          <NasaMdaCard/>
+        </div>
       </div>
+    </div>
   )
 }
 
 
-export default Slide_1
+export default Slide_4
