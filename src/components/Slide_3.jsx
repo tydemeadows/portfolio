4f30e3d5b4da3f7657bb9f7a1aad86a8673d668c
Tyde Meadows <tyde.meadows@gmail.com>
2022-08-21 08:30:54 -0500

    work on ksci page

--- src/components/Slide_3.jsx
+++ src/components/Slide_3.jsx
@@ -1,11 +1,17 @@
-import './Slide_1.css';
+import './Slide_3.css';
 import React, {useRef, useEffect, useState} from 'react'
 import ProjectCard from "./ProjectCard";
+import ksci_img from "./ksci_clean.png";
 
-const Slide_1 = () => {
+const Slide_3 = () => {
   return (
-    <div id="slide-1" className="slideobj">
-      <div id='pro-id' className="pro-holder">
+    <div id="slide-3" className="slideobj">
+      <div id='job_img_3'>
+          <a href="http://www.k-sciences.com/" target="_blank">
+          <img src={ksci_img} id="ksci_bg_img" className="pastbg" />
+          </a>
+        </div>
+      <div id='pro_id_3' className="pro_holder_3">
        <ProjectCard/>
        <ProjectCard/>
        <ProjectCard/>
@@ -15,4 +21,4 @@ const Slide_1 = () => {
 }
 
 
-export default Slide_1
+export default Slide_3
